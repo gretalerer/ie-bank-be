5ae69c4e1db546509f05c899329153308dff1a6d
Greta Lerer <gretalerer@gmail.com>
2023-10-14 22:43:02 +0200

    merching dev con main

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -7,9 +7,8 @@ def test_create_account():
     WHEN a new Account is created
     THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
     """
-    account = Account("John Doe", "Spain", "€")
+    account = Account('John Doe', '€')
     assert account.name == 'John Doe'
-    assert account.country == 'Spain'
     assert account.currency == '€'
     assert account.account_number != None
     assert account.balance == 0.0
