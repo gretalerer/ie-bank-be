5cb94a8b0dacbfa549772abd31ac3ac36951d38f
Greta Lerer <gretalerer@gmail.com>
2023-10-14 22:43:24 +0200

    merging test con dev (sin countries)

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -5,9 +5,8 @@ import string, random
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
-    country = db.Column(db.String(32), nullable=False)
     account_number = db.Column(db.String(20), nullable=False, unique=True)
-    balance = db.Column(db.Float, nullable=False, default=0.0)
+    balance = db.Column(db.Float, nullable=False, default = 0.0)
     currency = db.Column(db.String(1), nullable=False, default="€")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -15,10 +14,9 @@ class Account(db.Model):
     def __repr__(self):
         return '<Event %r>' % self.account_number
 
-    def __init__(self, name, country, currency):
+    def __init__(self, name, currency):
         self.name = name
-        self.country = country
-        self.account_number = "".join(random.choices(string.digits, k=20))
+        self.account_number = ''.join(random.choices(string.digits, k=20))
         self.currency = currency
         self.balance = 0.0
         self.status = "Active"
